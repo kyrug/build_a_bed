6c5872ccf7443bb1aec1b55d0dac277003ef4334
Chase Southard <chase.southard@gmail.com>
2009-10-30 15:57:09 -0400

    removed :on => create from job model

--- vendor/extensions/volunteers/app/models/job.rb
+++ vendor/extensions/volunteers/app/models/job.rb
@@ -2,10 +2,10 @@ class Job < ActiveRecord::Base
   has_many :volunteers
   
   # validates presence of name
-  validates_presence_of :name, :on => :create, :message => "can't be blank"
+  validates_presence_of :name, :message => "can't be blank"
 
   # validates the quantity greater than 0
-  validates_numericality_of :quatity, :on => :create, :only_integer => :true
-  validates_numericality_of :quatity, :on => :create, :greater_than_or_equal_to => 0, :message => "must be greater than or equal to 0"
+  validates_numericality_of :quatity, :only_integer => :true
+  validates_numericality_of :quatity, :greater_than_or_equal_to => 0, :message => "must be greater than or equal to 0"
 end
 
