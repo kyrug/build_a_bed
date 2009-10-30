d3d40d85f840e4beaed96c798db8af49e74c1e50
Chase Southard <chase.southard@gmail.com>
2009-10-30 15:58:11 -0400

    removed :on => :create from volunteer model validations

--- vendor/extensions/volunteers/app/models/volunteer.rb
+++ vendor/extensions/volunteers/app/models/volunteer.rb
@@ -6,16 +6,16 @@ class Volunteer < ActiveRecord::Base
   before_update :capitalize_names
   
   #validating name
-  validates_presence_of :first_name, :on => :create, :message => "can't be blank"
-  validates_presence_of :last_name, :on => :create, :message => "can't be blank"
+  validates_presence_of :first_name, :message => "can't be blank"
+  validates_presence_of :last_name, :message => "can't be blank"
   
   #validating email
-  validates_presence_of :email, :on => :create, :message => "can't be blank"
-  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
-  validates_uniqueness_of :email, :on => :create, :message => "already submitted; must be unique"
+  validates_presence_of :email, :message => "can't be blank"
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+  validates_uniqueness_of :email, :message => "already submitted; must be unique"
   
   #validating phone
-  validates_format_of :phone, :on => :create, :with => /^[+\/\-() 0-9]+$/, :message => 'is an invalid phone number, only the following characters are allowed: 0-9/-()+'
+  validates_format_of :phone, :with => /^[+\/\-() 0-9]+$/, :message => 'is an invalid phone number, only the following characters are allowed: 0-9/-()+'
   
   private
     def capitalize_names
