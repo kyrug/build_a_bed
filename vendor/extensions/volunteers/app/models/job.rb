d1e2b74831ca1141e3ebbfa793d431148c52ef32
Chase Southard <chase.southard@gmail.com>
2009-10-31 00:07:48 -0400

    fixed a typo. error messages do not display for some reason. hmm

--- vendor/extensions/volunteers/app/models/job.rb
+++ vendor/extensions/volunteers/app/models/job.rb
@@ -5,7 +5,7 @@ class Job < ActiveRecord::Base
   validates_presence_of :name, :message => "can't be blank"
 
   # validates the quantity greater than 0
-  validates_numericality_of :quatity, :only_integer => :true
-  validates_numericality_of :quatity, :greater_than_or_equal_to => 0, :message => "must be greater than or equal to 0"
+  validates_numericality_of :quantity, :only_integer => :true
+  validates_numericality_of :quantity, :greater_than_or_equal_to => 0, :message => "must be greater than or equal to 0"
 end
 
